3e9a5ba8a7d7bb46c5133d5d23d17167116c9489
Vladimir-create <79356244+Vladimir-create@users.noreply.github.com>
2021-03-08 20:59:49 +0200

    Add files via upload

--- client-TCP.go
+++ client-TCP.go
@@ -6,17 +6,26 @@ import (
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "127.0.0.1:12667")
+	conn, err := net.Dial("tcp", "127.0.0.1:12668")
 	if err != nil {
 		panic(err)
 	}
 	defer conn.Close()
 
 	s := "{\"action\":\"delete\",\"object\":\"Teacher\",\"data\":{\"id\":\"001\"}}"
-	conn.Write([]byte(s))
-    
+	conn.Write([]byte(s)
+	s2 := "{\"action\":\"create\",\"object\":\"Teacher\",\"data\":{\"id\":\"001\",\"subject\":\"Math\",\"salary\":2345,\"classroom\":[\"CL-001\",\"CL-002\",\"CL-005\"],\"person\":{\"name\":\"Ivan\",\"surname\":\"Popov\",\"personalCode\":\"123422-43235\"}}}"
+	conn.Write([]byte(s2)
+	s3 := "{\"action\":\"create\",\"object\":\"Teacher\",\"data\":{\"id\":\"0ds01\",\"subject\":\"Math\",\"salary\":2345,\"classroom\":[\"CL-001\",\"CL-002\",\"CL-005\"],\"person\":{\"name\":\"Ivan\",\"surname\":\"Popov\",\"personalCode\":\"123422-43235\"}}}"
+	conn.Write([]byte(s3)
+	s4 := "{\"action\":\"read\",\"object\":\"Teacher\",\"data\":{\"id\":\"001\"}}"
+	conn.Write([]byte(s4)
+	s5 := "{\"action\":\"update\",\"object\":\"Teacher\",\"data\":{\"id\":\"001\",\"subject\":\"Math\",\"salary\":2345,\"classroom\":[\"CL-001\",\"CL-002\",\"CL-005\"],\"person\":{\"name\":\"Ivan\",\"surname\":\"nigga\",\"personalCode\":\"123422-43235\"}}}"
+	conn.Write([]byte(s5)
+	s6 := "{\"action\":\"delete\",\"object\":\"Teacher\",\"data\":{\"id\":\"001\"}}"
+	conn.Write([]byte(s6)
+	
 	buf := make([]byte, 2000)
-
 	n, err := conn.Read(buf)
 	if err != nil {
 		panic(err)
--- server_TCP.go
+++ server_TCP.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net"
 	"fmt"
+	"io"
 	"encoding/json"
 )
 
@@ -12,7 +13,6 @@ type (
 		ObjName string `json:"object"`
 	}
 	Teacher struct {
-		Ch chan int
 		ID string  `json:"id"`
 		Salary float64 `json:"salary"`
 		Subject string `json:"subject"`
@@ -22,6 +22,20 @@ type (
 			Surname string `json:"surname"`
 			PersonalCode string `json:"personalCode"`
 		} `json:"person"`
+	}
+	Teachermutex struct {
+		Ch chan int
+	}
+	UpdateTeacher struct {
+		T Teacher `json:"data"`
+	}
+	CreateTeacher struct {
+		T Teacher `json:"data"`
+	}
+	DeleteTeacher struct {
+		Data struct {
+			ID string `json:"id"`
+		} `json:"data"`
 	}
 	ReadTeacher struct {
 		Data struct {
@@ -36,22 +50,71 @@ type (
 		Process()
 	}
 	GeneralObject interface {
+		GetCreateAction() DefinedAction
+		GetUpdateAction() DefinedAction
 		GetReadAction() DefinedAction
+		GetDeleteAction() DefinedAction
 		Read(str string)bool
 		Print()
 	}
 )
+
+func (t Teacher) GetCreateAction() DefinedAction {
+	return &CreateTeacher{}
+}
+func (t Teacher) GetUpdateAction() DefinedAction {
+	return &UpdateTeacher{}
+}
 func (t Teacher) GetReadAction() DefinedAction {
 	return &ReadTeacher{}
+}
+func (t Teacher) GetDeleteAction() DefinedAction {
+	return &DeleteTeacher{}
 }
 
 func (action ReadTeacher) Process() {
 	fmt.Println("Read teacher", action.Data.ID)
 	for i:=0;i<len(arriPerson);i++{
 		if arriPerson[i].Read(action.Data.ID) {
+			<- teachermutex.Ch
 			arriPerson[i].Print()
 		}
 	}
+	teachermutex.Ch <- 1
+}
+
+func (action CreateTeacher) Process(){
+	teachermutex.Ch = make(chan int, 1)
+	teachermutex.Ch <- 1
+	fmt.Println("Create Teacher")
+	arriPerson = append(arriPerson, &action.T)
+	PrintAll(arriPerson)
+}
+
+func (action UpdateTeacher) Process() {
+	fmt.Println("Update Teachers")
+	for i:=0;i<len(arriPerson);i++{	
+		if arriPerson[i].Read(action.T.ID) {
+			<- teachermutex.Ch
+			arriPerson[i] = &action.T
+		}
+	}
+	PrintAll(arriPerson)
+	teachermutex.Ch <- 1
+}
+
+func (action DeleteTeacher) Process() {
+	fmt.Println("Teacher deleted", action.Data.ID)
+	for i:=0;i<len(arriPerson);i++{
+		if arriPerson[i].Read(action.Data.ID) {
+			<- teachermutex.Ch
+			copy(arriPerson[i:], arriPerson[i+1:])
+			arriPerson[len(arriPerson)-1] = nil
+			arriPerson = arriPerson[:len(arriPerson)-1]
+		}
+	}
+	PrintAll(arriPerson)
+	teachermutex.Ch <- 1
 }
 
 func (i *Teacher) Print(){
@@ -67,7 +130,6 @@ func (i *Teacher) Print(){
 	fmt.Println()
 }
 
-
 func (i *Teacher) Read(str string) bool{
 	return i.ID == str
 }
@@ -79,25 +141,50 @@ func (action *ReadTeacher) GetFromJSON (rawData []byte) {
 		return
 	}
 }
+
+func (action *DeleteTeacher) GetFromJSON (rawData []byte) {
+	err := json.Unmarshal(rawData, action)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+
+func (action *UpdateTeacher) GetFromJSON (rawData []byte) {
+	err := json.Unmarshal(rawData, action)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+
+func (action *CreateTeacher) GetFromJSON (rawData []byte) {
+	err := json.Unmarshal(rawData, action)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
 	
-var teacher Teacher 
+func PrintAll(arriPerson []GeneralObject){
+	for i:=0; i<len(arriPerson); i++{
+		arriPerson[i].Print()
+	}	
+}
+
+var teachermutex Teachermutex
 func main() {
-	l, err := net.Listen("tcp", "127.0.0.1:12667")
+	l, err := net.Listen("tcp", "127.0.0.1:12668")
 	if err != nil {
 		panic(err)
 	}
 	defer l.Close()
-
-	teacher.Ch = make(chan int, 1)
-	teacher.Ch <- 1
-
 	for {
 		conn, err := l.Accept()
 		if err != nil {
 			fmt.Println(err)
 			continue
 		}
-
 		go HandleConnection(conn)
 	}
 }
@@ -105,19 +192,22 @@ func main() {
 var arriPerson []GeneralObject
 
 func HandleConnection(conn net.Conn) {
-	buf := make([]byte, 2000)
-	n, err := conn.Read(buf)
-	if err != nil {
-		conn.Close()
-		return
-	}
+	buf := make([]byte, 0, 10000) 
+    smallBuf := make([]byte, 256)    
+    for {
+        n, err := conn.Read(smallBuf)
+        if err != nil {
+            if err != io.EOF {
+                fmt.Println(err)
+            }
+            break
+        }
+        buf = append(buf, smallBuf[:n]...)
+    }
 	var act Action
 	var obj GeneralObject
 	var toDo DefinedAction
-	
-	<- teacher.Ch
-	
-	err = json.Unmarshal(buf[:n], &act)
+	err := json.Unmarshal(buf[:len(buf)], &act)
 	if err != nil {
 		fmt.Println("error")
 	}
@@ -126,18 +216,18 @@ func HandleConnection(conn net.Conn) {
 		obj = &Teacher{}
 	}
 	switch act.Action {
-	case "read":
-		toDo = obj.GetReadAction()
+		case "create":
+			toDo = obj.GetCreateAction()
+		case "update":
+			toDo = obj.GetUpdateAction()
+		case "read":
+			toDo = obj.GetReadAction()
+		case "delete":
+			toDo = obj.GetDeleteAction()
 	}
-	toDo.GetFromJSON(buf[:n])
+	toDo.GetFromJSON(buf[:len(buf)])
 	toDo.Process()
-	//fmt.Println(string(buf[:n]))
-	//dat.N = string(buf[:n])
-
 	data := []byte("Connection great")
 	conn.Write(data)
-
-	teacher.Ch <- 1
-
 	conn.Close()
 }
