e7c73492b20e43fa5d3975144af8f0208ec433ee
Vladimir-create <79356244+Vladimir-create@users.noreply.github.com>
2021-03-13 10:42:39 +0200

    Add files via upload

--- client-TCP.go
+++ client-TCP.go
@@ -13,17 +13,17 @@ func main() {
 	defer conn.Close()
 
 	s := "{\"action\":\"delete\",\"object\":\"Teacher\",\"data\":{\"id\":\"001\"}}"
-	conn.Write([]byte(s)
+	conn.Write([]byte(s))
 	s2 := "{\"action\":\"create\",\"object\":\"Teacher\",\"data\":{\"id\":\"001\",\"subject\":\"Math\",\"salary\":2345,\"classroom\":[\"CL-001\",\"CL-002\",\"CL-005\"],\"person\":{\"name\":\"Ivan\",\"surname\":\"Popov\",\"personalCode\":\"123422-43235\"}}}"
-	conn.Write([]byte(s2)
+	conn.Write([]byte(s2))
 	s3 := "{\"action\":\"create\",\"object\":\"Teacher\",\"data\":{\"id\":\"0ds01\",\"subject\":\"Math\",\"salary\":2345,\"classroom\":[\"CL-001\",\"CL-002\",\"CL-005\"],\"person\":{\"name\":\"Ivan\",\"surname\":\"Popov\",\"personalCode\":\"123422-43235\"}}}"
-	conn.Write([]byte(s3)
+	conn.Write([]byte(s3))
 	s4 := "{\"action\":\"read\",\"object\":\"Teacher\",\"data\":{\"id\":\"001\"}}"
-	conn.Write([]byte(s4)
+	conn.Write([]byte(s4))
 	s5 := "{\"action\":\"update\",\"object\":\"Teacher\",\"data\":{\"id\":\"001\",\"subject\":\"Math\",\"salary\":2345,\"classroom\":[\"CL-001\",\"CL-002\",\"CL-005\"],\"person\":{\"name\":\"Ivan\",\"surname\":\"nigga\",\"personalCode\":\"123422-43235\"}}}"
-	conn.Write([]byte(s5)
+	conn.Write([]byte(s5))
 	s6 := "{\"action\":\"delete\",\"object\":\"Teacher\",\"data\":{\"id\":\"001\"}}"
-	conn.Write([]byte(s6)
+	conn.Write([]byte(s6))
 	
 	buf := make([]byte, 2000)
 	n, err := conn.Read(buf)
